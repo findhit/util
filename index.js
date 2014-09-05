4c2c43a0da610247f90a587a8cba8bda5e75cfe1
José Moreira <jmoreira@microdual.com>
2014-09-06 00:54:18 +0100

    Util, remove fn

--- index.js
+++ index.js
@@ -14,14 +14,16 @@
  *
  * @class Util
  */
-module.exports = (function () {
-	var Util = {};
 
-	require('./lib/core')( Util );
-	require('./lib/function')( Util );
-	require('./lib/is-isnt')( Util );
-	require('./lib/from')( Util );
-	require('./lib/to')( Util );
+var Util = {};
 
-	return Util;
-})();
+require('./lib/core')( Util );
+require('./lib/function')( Util );
+require('./lib/is-isnt')( Util );
+require('./lib/from')( Util );
+require('./lib/to')( Util );
+
+// Export it
+module.exports = Util;
+
+// TODO: AMDify
