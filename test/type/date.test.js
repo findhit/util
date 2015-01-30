dd837dd344b4eec3d21408b50cae337b77e98e80
António Loureiro <ajb.loureiro@gmail.com>
2015-01-30 15:56:12 +0000

    fixed "Error: global leak detected: t"

--- test/type/date.test.js
+++ test/type/date.test.js
@@ -13,18 +13,18 @@ describe( "ƒ", function () {
             it( "formats new Date(2013,0,1) (1st January 2013) into 'Y/m/d'", function () {
                 var currentDate = new Date(2013,0,1);
 
-                t = ƒ.date.format( currentDate, 'Y/m/d' );
-
-                expect( t ).to.be.equal( '2013/01/01' );
+                expect(
+                    ƒ.date.format( currentDate, 'Y/m/d' )
+                ).to.be.equal( '2013/01/01' );
 
             });
 
             it( "formats new Date(2013,0,1) (1st January 2013) into 'd-m-Y'", function () {
                 var currentDate = new Date(2013,0,1);
 
-                t = ƒ.date.format( currentDate, 'd-m-Y' );
-
-                expect( t ).to.be.equal( '01-01-2013' );
+                expect(
+                    ƒ.date.format( currentDate, 'd-m-Y' )
+                ).to.be.equal( '01-01-2013' );
 
             });
 
